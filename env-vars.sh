553dd6a097d240496289e089190e84be39ae6445
chip-dev <112027862+cunicopia-dev@users.noreply.github.com>
2024-04-17 15:16:51 -0400

    Create env-vars.sh

---
+++ env-vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export OPENAI_API_KEY="dummykey"
+export OPENAI_API_BASE="http://localhost:11434/v1"
+export OPENAI_MODEL_NAME="llama2:13b-chat-q8_0"
+export SERPER_API_KEY="12ec3cdbe21024e53f635d5675281be3ce935698"
